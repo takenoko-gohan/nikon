af4e5e0f8b3666a410ebbb892915cc8e8851306c
takenoko-gohan <bamboo.shoot.rice@gmail.com>
2021-01-09 04:50:05 +0000

    Added the ability to cancel goroutine.

--- internal/dump/saving_index.go
+++ internal/dump/saving_index.go
@@ -5,6 +5,7 @@ package dump
 
 import (
 	//"context"
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -43,7 +44,7 @@ func SavingIndex(addr string, iName string, size int, t int, o string) {
 		os.Exit(0)
 	}
 
-	var eg errgroup.Group
+	eg, ctx := errgroup.WithContext(context.Background())
 
 	chRes := make(chan map[string]interface{}, 10)
 	chDoc := make(chan []map[string]string, 10)
@@ -58,31 +59,46 @@ func SavingIndex(addr string, iName string, size int, t int, o string) {
 	var mu1 sync.Mutex
 
 	eg.Go(func() error {
-		mu1.Lock()
-		defer mu1.Unlock()
-		defer close(chRes)
-		for i := 0; i < cnt; i++ {
-			offset := (i + 1) * size
-			err := getScrollRes(es, iName, scrollID, t, offset, chRes)
-			if err != nil {
-				return err
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			mu1.Lock()
+			defer mu1.Unlock()
+			defer close(chRes)
+			for i := 0; i < cnt; i++ {
+				offset := (i + 1) * size
+				err := getScrollRes(es, iName, scrollID, t, offset, chRes)
+				if err != nil {
+					return err
+				}
 			}
-		}
 
-		return nil
+			return nil
+		}
 	})
 
 	eg.Go(func() error {
-		defer close(chDoc)
-		return processing.GetDocsData(chRes, chDoc)
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			defer close(chDoc)
+			return processing.GetDocsData(chRes, chDoc)
+		}
 	})
 
 	var mu2 sync.Mutex
 
 	eg.Go(func() error {
-		mu2.Lock()
-		defer mu2.Unlock()
-		return saveDocToFile(o, chDoc)
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			mu2.Lock()
+			defer mu2.Unlock()
+			return saveDocToFile(o, chDoc)
+		}
 	})
 
 	if err := eg.Wait(); err != nil {
